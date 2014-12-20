8f655e924d8441583ce3375da3d3f360e62c9f8e
Ken Allen <chunkman@gmail.com>
2014-12-20 02:44:56 -0500

    You should not see this

--- ex13.py
+++ ex13.py
@@ -2,13 +2,9 @@ from sys import argv
 
 script, first, second, third = argv
 
-#print "The script is called:", script
-#print "Your first variable is:", first
-#print "Your second variable is:", second
-#print "Your third variable is:", third
-
 name = raw_input("What is your name? ")
 age = raw_input("What is your age? ")
 gender = raw_input("What is your gender? ")
 print "Hello, my name is %s and I'm a %s year old %s who likes %s, %s and %s." % (
         name, age, gender, first, second, third)
+print "This is just a test branch. You should not see this."

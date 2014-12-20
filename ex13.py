70b0905c89475937b4339230610bf2acaaaef02d
Ken Allen <chunkman@gmail.com>
2014-12-20 02:49:14 -0500

    Deleted unneeded statements in ex13

--- ex13.py
+++ ex13.py
@@ -7,4 +7,3 @@ age = raw_input("What is your age? ")
 gender = raw_input("What is your gender? ")
 print "Hello, my name is %s and I'm a %s year old %s who likes %s, %s and %s." % (
         name, age, gender, first, second, third)
-print "This is just a test branch. You should not see this."

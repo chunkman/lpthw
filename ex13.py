4be2988d1d71bd89b39f6d0c0d30048b50099c69
Ken Allen <chunkman@gmail.com>
2014-12-20 02:34:41 -0500

    Testing new features of ex13

--- ex13.py
+++ ex13.py
@@ -2,7 +2,13 @@ from sys import argv
 
 script, first, second, third = argv
 
-print "The script is called:", script
-print "Your first variable is:", first
-print "Your second variable is:", second
-print "Your third variable is:", third
+#print "The script is called:", script
+#print "Your first variable is:", first
+#print "Your second variable is:", second
+#print "Your third variable is:", third
+
+name = raw_input("What is your name? ")
+age = raw_input("What is your age? ")
+gender = raw_input("What is your gender? ")
+print "Hello, my name is %s and I'm a %s year old %s who likes %s, %s and %s." % (
+        name, age, gender, first, second, third)

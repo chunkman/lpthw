792888ca92937c0f2fd161025d72f57e19b04634
Ken Allen <chunkman@gmail.com>
2015-04-15 01:32:43 -0400

    Modified ex38

--- ex38.py
+++ ex38.py
@@ -2,14 +2,20 @@ ten_things = "Apples Oranges Crows Telephone Light Sugar"
 
 print "Wait there are not 10 things in that list. Let's fix that."
 
+max_list = 10
+
 stuff = ten_things.split(' ')
 more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
 
-while len(stuff) != 10:
+for i in range(0, max_list - len(stuff)):
     next_one = more_stuff.pop()
     print "Adding: ", next_one
     stuff.append(next_one)
     print "There are %d items now." % len(stuff)
+
+    if len(more_stuff) == 0:
+        print "Ran out of items to add."
+        break
 
 print "There we go: ", stuff
 
